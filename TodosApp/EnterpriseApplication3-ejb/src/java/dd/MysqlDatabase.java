f9d1b2de199de6f04cfff5862afe407d9cd2fcc8
younesben011 <younesbenzaama011@gmail.com>
2023-12-04 19:47:02 +0100

    setup database using Room create the tables and dao interface, the finally create ui according to the database

---
+++ TodosApp/EnterpriseApplication3-ejb/src/java/dd/MysqlDatabase.java
@@ -0,0 +1,37 @@
+package dd;
+
+import java.sql.*;
+
+public class MysqlDatabase {
+
+    private MysqlDatabase(){
+    }
+
+    public static Connection getConnection()throws SQLException {
+        Connection connection=null;
+        String url = "jdbc:mysql://localhost:3306/vols";
+        String username="root";
+        String password="054090";
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            connection = DriverManager.getConnection(url,username,password);
+
+        } catch (ClassNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+        return connection;
+    }
+    public static  void  close(Connection connection ,PreparedStatement stP) throws SQLException{
+        closePreSt(stP);
+        closeConnection(connection);
+    }
+    public  static void  closeConnection(Connection connection) throws SQLException{
+        connection.close();
+    }
+    public static  void  closeSt(Statement st)throws SQLException{
+        st.close();
+    }
+    public static void  closePreSt(PreparedStatement st) throws SQLException{
+        st.close();
+    }
+}
